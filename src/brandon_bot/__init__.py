f9d6fb67f713c727f9e6847104273c6974afaf89
Btom <btom5693@gmail.com>
2025-09-15 16:06:22 -0700

    Fix import error: update __init__.py to use chat_interface_simple

--- src/brandon_bot/__init__.py
+++ src/brandon_bot/__init__.py
@@ -6,7 +6,7 @@ __version__ = "0.1.0"
 
 from .bot import resume_bot
 from .document_processor import document_processor
-from .chat_interface import create_interface
+from .chat_interface_simple import create_interface
 from .config import config
 # All analytics handled by OpenAI natively
 
